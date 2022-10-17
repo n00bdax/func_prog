c7e57803858c82ddc4f6b0aebd3a02465460480a
n00bdax <jschwndngr@gmail.com>
2022-10-17 04:14:12 +0200

    A1 done, but entirely with nested functions

--- src/Angabe1.hs
+++ src/Angabe1.hs
@@ -22,25 +22,40 @@ type Histogramm   = [(Zeichen,Haeufigkeit)]
 
 type Gewicht        = Nat0
 type Gewichtsverzeichnis = [(Zeichen,Gewicht)]
-fehlerwert :: Gewicht -- why wasn't this type declared already?
+--fehlerwert :: Gewicht -- why wasn't this type declared already?
 fehlerwert = -1
 
 -- Aufgabe A.1
 
 
-histAdd :: Zeichen -> Histogramm -> Histogramm
-histAdd c h
-  | null h = [(c,1)]
-  | c == fst (head h) = (fst (head h) ,snd (head h) +1):tail h
-  | otherwise = head h : histAdd c (tail h)
+
+{-
+haeufigkeit :: Zeichenreihe -> Histogramm
+haeufigkeit s = extractChars s []::Histogramm
 
 extractChars :: Zeichenreihe -> Histogramm -> Histogramm
 extractChars s h
   | null s = h
   | otherwise = extractChars (tail s) (histAdd (head s) h)
 
+histAdd :: Zeichen -> Histogramm -> Histogramm
+histAdd c h
+  | null h = [(c,1)]
+  | c == fst (head h) = (fst (head h) ,snd (head h) +1):tail h
+  | otherwise = head h : histAdd c (tail h)
+-}
+
 haeufigkeit :: Zeichenreihe -> Histogramm
-haeufigkeit s = extractChars s []::Histogramm
+haeufigkeit s = let
+  extractChars :: Zeichenreihe -> Histogramm -> Histogramm
+  extractChars [] h = h
+  extractChars s h = let histAdd :: Zeichen -> Histogramm -> Histogramm
+                         histAdd c h
+                          | null h = [(c,1)]
+                          | c == fst (head h) = (fst (head h) ,snd (head h) +1):tail h
+                          | otherwise = head h : histAdd c (tail h)
+    in extractChars (tail s) (histAdd (head s) h)
+  in extractChars s []::Histogramm
 
 {- Knapp, aber gut nachvollziehbar geht haufigkeit folgendermassen vor:
    
@@ -55,18 +70,36 @@ haeufigkeit s = extractChars s []::Histogramm
 
 -- Aufgabe A.2
 
+
+{-
+gewicht:: Zeichenreihe -> Gewichtsverzeichnis -> Gewicht
+gewicht z g
+  |not (registerDuplicateCheck g) = fehlerwert
+  | otherwise = sum[snd a | a<-g, b<-z , fst a == b]
+
+registerDuplicateCheck :: Gewichtsverzeichnis -> Bool
+registerDuplicateCheck g = stringDuplicateCheck[fst x | x <- g ]
+
 stringDuplicateCheck :: Zeichenreihe -> Bool
 stringDuplicateCheck g
   | length g <= 1 = True
   | head g `elem` tail g = False
   | otherwise = stringDuplicateCheck (tail g)
+-}
 
-registerDuplicateCheck :: Gewichtsverzeichnis -> Bool
-registerDuplicateCheck g = stringDuplicateCheck[fst x | x <- g ]
+
 
 gewicht:: Zeichenreihe -> Gewichtsverzeichnis -> Gewicht
 gewicht z g
-  | not (registerDuplicateCheck g) = fehlerwert
+  | let registerDuplicateCheck :: Gewichtsverzeichnis -> Bool
+        registerDuplicateCheck g =
+          let stringDuplicateCheck :: Zeichenreihe -> Bool
+              stringDuplicateCheck g
+                | length g <= 1 = True
+                | head g `elem` tail g = False
+                | otherwise = stringDuplicateCheck (tail g)
+          in stringDuplicateCheck[fst x | x <- g ]
+  in not (registerDuplicateCheck g) = fehlerwert
   | otherwise = sum[snd a | a<-g, b<-z , fst a == b]
 
 
@@ -78,15 +111,25 @@ gewicht z g
 
 
 -- Aufgabe A.3
+{-
+korrigiere :: Gewichtsverzeichnis -> Gewichtsverzeichnis
+korrigiere g = fixRegister [head g] (tail g)
 
 fixRegister :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
 fixRegister a b
   | null b = a
   | fst (head b) `elem` ([fst x | x<-a]) = fixRegister a (tail b)
   | otherwise = fixRegister (a++ [head b]) (tail b)
+-}
+
 
 korrigiere :: Gewichtsverzeichnis -> Gewichtsverzeichnis
 korrigiere g = fixRegister [head g] (tail g)
+  where fixRegister :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+        fixRegister a b
+          | null b = a
+          | fst (head b) `elem` ([fst x | x<-a]) = fixRegister a (tail b)
+          | otherwise = fixRegister (a++ [head b]) (tail b)
 
 {- Knapp, aber gut nachvollziehbar geht gewicht folgendermassen vor:
 
@@ -98,14 +141,9 @@ if <betrachtetes Element> in <Anfang> then cut it out
 
 -- Aufgabe A.4
 
---korrigiere' :: Gewichtsverzeichnis -> Gewichtsverzeichnis
-sumUp :: (Zeichen, Haeufigkeit) -> Gewichtsverzeichnis -> Gewichtsverzeichnis
-sumUp a b = [(fst a, snd a + sum[snd x | x <- b , fst x == fst a])]
-
-fixRegister'Deep:: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
-fixRegister'Deep a b
-  | null b = a
-  | otherwise = fixRegister' (init a ++ sumUp (last a) b) b
+{-
+korrigiere' :: Gewichtsverzeichnis -> Gewichtsverzeichnis
+korrigiere' g = fixRegister'Deep [head g] (tail g)
 
 fixRegister' :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
 fixRegister' a b
@@ -113,8 +151,33 @@ fixRegister' a b
   | fst (head b) `elem` ([fst x | x<-a]) = fixRegister' a (tail b)
   | otherwise = fixRegister'Deep (a ++ [head b]) (tail b)
 
+fixRegister'Deep:: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+fixRegister'Deep a b
+  | null b = a
+  | otherwise = fixRegister' (init a ++ sumUp (last a) b) b
+
+sumUp :: (Zeichen, Haeufigkeit) -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+sumUp a b = [(fst a, snd a + sum[snd x | x <- b , fst x == fst a])]
+-}
+
+
+
 korrigiere' :: Gewichtsverzeichnis -> Gewichtsverzeichnis
-korrigiere' g = fixRegister'Deep [head g] (tail g)
+korrigiere' g =
+  let fixRegister'Deep:: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+      fixRegister'Deep a b
+        | null b = a
+        | otherwise =
+        let fixRegister' :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+            fixRegister' a b
+              | null b = a
+              | fst (head b) `elem` ([fst x | x<-a]) = fixRegister' a (tail b)
+              | otherwise = fixRegister'Deep (a ++ [head b]) (tail b)
+        in let
+        sumUp :: (Zeichen, Haeufigkeit) -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+        sumUp a b = [(fst a, snd a + sum[snd x | x <- b , fst x == fst a])]
+        in fixRegister' (init a ++ sumUp (last a) b) b
+  in fixRegister'Deep [head g] (tail g)
 
 {- Knapp, aber gut nachvollziehbar geht gewicht folgendermassen vor:
 
@@ -125,27 +188,27 @@ auf das zuletzt betrachtete Element addiert
 
 -- Test Variables
 --
---masses :: Gewichtsverzeichnis
---masses = [('m',1),('a',2),('s',3),('s',4),('e',5),('s',6)]
---
---masseses :: Gewichtsverzeichnis
---masseses = masses ++ masses
---
---pl::Gewichtsverzeichnis
---pl = [('a',2),('b',1),('c',3),('d',2),('e',3)]
---
---mss::Gewichtsverzeichnis
---mss=[('a',0),('l',1)]
---
---mnn::Gewichtsverzeichnis
---mnn= [('a',2),('b',1),('c',3),('d',2),('e',3),('a',2),('b',1),('c',3),('d',2),('e',3)]
---
---ad14 :: Gewichtsverzeichnis
---ad14 = [(a,b)|b<-[1..4],a<-['a'..'d']]
---
---s1 :: Zeichenreihe
---s1 = "The quick brown fox jumps over the lazy dog"::Zeichenreihe
---
---s2 :: Zeichenreihe
---s2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." :: Zeichenreihe
+masses :: Gewichtsverzeichnis
+masses = [('m',1),('a',2),('s',3),('s',4),('e',5),('s',6)]
+
+masseses :: Gewichtsverzeichnis
+masseses = masses ++ masses
+
+pl::Gewichtsverzeichnis
+pl = [('a',2),('b',1),('c',3),('d',2),('e',3)]
+
+mss::Gewichtsverzeichnis
+mss=[('a',0),('l',1)]
+
+mnn::Gewichtsverzeichnis
+mnn= [('a',2),('b',1),('c',3),('d',2),('e',3),('a',2),('b',1),('c',3),('d',2),('e',3)]
+
+ad14 :: Gewichtsverzeichnis
+ad14 = [(a,b)|b<-[1..4],a<-['a'..'d']]
+
+s1 :: Zeichenreihe
+s1 = "The quick brown fox jumps over the lazy dog"::Zeichenreihe
+
+s2 :: Zeichenreihe
+s2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." :: Zeichenreihe
 
