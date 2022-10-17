dd00580244e3b0717e911a41cc7ccaa488ceb8cb
n00bdax <jschwndngr@gmail.com>
2022-10-17 04:56:14 +0200

    A1 done, but entirely with where

--- src/Angabe1.hs
+++ src/Angabe1.hs
@@ -22,7 +22,7 @@ type Histogramm   = [(Zeichen,Haeufigkeit)]
 
 type Gewicht        = Nat0
 type Gewichtsverzeichnis = [(Zeichen,Gewicht)]
---fehlerwert :: Gewicht -- why wasn't this type declared already?
+fehlerwert :: Int-- why wasn't this type declared already?
 fehlerwert = -1
 
 -- Aufgabe A.1
@@ -46,16 +46,18 @@ histAdd c h
 -}
 
 haeufigkeit :: Zeichenreihe -> Histogramm
-haeufigkeit s = let
+haeufigkeit s =  extractChars s []::Histogramm
+  where
   extractChars :: Zeichenreihe -> Histogramm -> Histogramm
   extractChars [] h = h
-  extractChars s h = let histAdd :: Zeichen -> Histogramm -> Histogramm
-                         histAdd c h
-                          | null h = [(c,1)]
-                          | c == fst (head h) = (fst (head h) ,snd (head h) +1):tail h
-                          | otherwise = head h : histAdd c (tail h)
-    in extractChars (tail s) (histAdd (head s) h)
-  in extractChars s []::Histogramm
+  extractChars remaining h = extractChars (tail remaining) (histAdd (head remaining) h)
+
+  histAdd :: Zeichen -> Histogramm -> Histogramm
+  histAdd c h
+    | null h = [(c,1)]
+    | c == fst (head h) = (fst (head h) ,snd (head h) +1):tail h
+    | otherwise = head h : histAdd c (tail h)
+
 
 {- Knapp, aber gut nachvollziehbar geht haufigkeit folgendermassen vor:
    
@@ -91,16 +93,21 @@ stringDuplicateCheck g
 
 gewicht:: Zeichenreihe -> Gewichtsverzeichnis -> Gewicht
 gewicht z g
-  | let registerDuplicateCheck :: Gewichtsverzeichnis -> Bool
-        registerDuplicateCheck g =
-          let stringDuplicateCheck :: Zeichenreihe -> Bool
-              stringDuplicateCheck g
-                | length g <= 1 = True
-                | head g `elem` tail g = False
-                | otherwise = stringDuplicateCheck (tail g)
-          in stringDuplicateCheck[fst x | x <- g ]
-  in not (registerDuplicateCheck g) = fehlerwert
+  | not (registerDuplicateCheck g) = fehlerwert
   | otherwise = sum[snd a | a<-g, b<-z , fst a == b]
+
+  where
+  stringDuplicateCheck :: Zeichenreihe -> Bool
+  stringDuplicateCheck r
+    | length r <= 1 = True
+    | head r `elem` tail r = False
+    | otherwise = stringDuplicateCheck (tail r)
+
+  registerDuplicateCheck :: Gewichtsverzeichnis -> Bool
+  registerDuplicateCheck r = stringDuplicateCheck[fst x | x <- r ]
+
+
+
 
 
 
@@ -125,11 +132,12 @@ fixRegister a b
 
 korrigiere :: Gewichtsverzeichnis -> Gewichtsverzeichnis
 korrigiere g = fixRegister [head g] (tail g)
-  where fixRegister :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
-        fixRegister a b
-          | null b = a
-          | fst (head b) `elem` ([fst x | x<-a]) = fixRegister a (tail b)
-          | otherwise = fixRegister (a++ [head b]) (tail b)
+  where
+  fixRegister :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+  fixRegister a b
+    | null b = a
+    | fst (head b) `elem` ([fst x | x<-a]) = fixRegister a (tail b)
+    | otherwise = fixRegister (a++ [head b]) (tail b)
 
 {- Knapp, aber gut nachvollziehbar geht gewicht folgendermassen vor:
 
@@ -163,21 +171,21 @@ sumUp a b = [(fst a, snd a + sum[snd x | x <- b , fst x == fst a])]
 
 
 korrigiere' :: Gewichtsverzeichnis -> Gewichtsverzeichnis
-korrigiere' g =
-  let fixRegister'Deep:: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
-      fixRegister'Deep a b
-        | null b = a
-        | otherwise =
-        let fixRegister' :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
-            fixRegister' a b
-              | null b = a
-              | fst (head b) `elem` ([fst x | x<-a]) = fixRegister' a (tail b)
-              | otherwise = fixRegister'Deep (a ++ [head b]) (tail b)
-        in let
-        sumUp :: (Zeichen, Haeufigkeit) -> Gewichtsverzeichnis -> Gewichtsverzeichnis
-        sumUp a b = [(fst a, snd a + sum[snd x | x <- b , fst x == fst a])]
-        in fixRegister' (init a ++ sumUp (last a) b) b
-  in fixRegister'Deep [head g] (tail g)
+korrigiere' g =  fixRegister'Deep [head g] (tail g)
+  where
+  fixRegister' a b
+    | null b = a
+    | fst (head b) `elem` ([fst x | x<-a]) = fixRegister' a (tail b)
+    | otherwise = fixRegister'Deep (a ++ [head b]) (tail b)
+
+  fixRegister'Deep:: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+  fixRegister'Deep a b
+    | null b = a
+    | otherwise = fixRegister' (init a ++ sumUp (last a) b) b
+  fixRegister' :: Gewichtsverzeichnis -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+
+  sumUp :: (Zeichen, Haeufigkeit) -> Gewichtsverzeichnis -> Gewichtsverzeichnis
+  sumUp a b = [(fst a, snd a + sum[snd x | x <- b , fst x == fst a])]
 
 {- Knapp, aber gut nachvollziehbar geht gewicht folgendermassen vor:
 
@@ -186,6 +194,7 @@ auf das zuletzt betrachtete Element addiert
 
 -}
 
+{-
 -- Test Variables
 --
 masses :: Gewichtsverzeichnis
@@ -211,4 +220,5 @@ s1 = "The quick brown fox jumps over the lazy dog"::Zeichenreihe
 
 s2 :: Zeichenreihe
 s2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." :: Zeichenreihe
+-}
 
