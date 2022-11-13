975d9ed1bac47a5be4350ba2d2d756d0d4d4b58f
n00bdax <n00bdax@hotmail.com>
2022-11-13 05:42:12 +0100

    A.5 wrote test case data, yeet to try it, time to watch interlgagos sprint quali, it's 5:42 ffs

--- src/Angabe5.hs
+++ src/Angabe5.hs
@@ -76,17 +76,19 @@ class Wgf a where                  -- Wgf fuer `wohlgeformt'
 
 
 
--- global helper function
 
-list :: [Haendler]
-list = reverse [minBound..maxBound]
+
+
+
+
+-- global helper function
 
 
 hasDuplicates :: Eq a => [a] -> Bool
 hasDuplicates (x:xs) = elem x xs || hasDuplicates xs
 hasDuplicates _      = False
 
--- made them thinking they make sense, never usd them
+-- made them thinking they make sense, never used them
 matchL :: Eq a => a -> [(a,b)] -> Maybe b
 matchL a b
    | length bs > 1 = error "uncaught error, fix me above matchL"
@@ -209,8 +211,8 @@ sofort_lieferfaehig :: Suchanfrage -> Anbieter -> Haendlerliste
 sofort_lieferfaehig typ a
    | ist_nwgf a = wgf_fehler a
    | otherwise = map fst . filter (\(_,x) -> gStock x > 0) $ toData typ a
-      
-      
+
+
 --        map fst $ filter (hasIt typ . snd) a
 --    where
 
@@ -238,7 +240,7 @@ sofort_erhaeltliche_Stueckzahl typ anbieter
    | otherwise = foldl (\(a,b)(c,d)->(a+c,b+d))(0,0) . map(\(_,x)-> (gPrice x,gPrice x *  gStock x)) $ toData typ anbieter
 
 
-   
+
 -- sofort_erhaeltliche_Stueckzahl :: Suchanfrage -> Anbieter -> (Stueckzahl,Gesamtpreis)
 -- sofort_erhaeltliche_Stueckzahl typ a
 --    | ist_nwgf a = wgf_fehler a
@@ -289,11 +291,183 @@ guenstigste_Lieferanten_im_Lieferfenster typ lff n a
                   map (\(x,y) -> (x,EUR $ ceiling (gPriceRed y * fromIntegral n))) .
                   filter (\(_,x) -> gStockBy x lff >= n) $
                   toData typ a
-                  
+
 
 {- Knapp, aber gut nachvollziehbar geht die Implementierung olgendermassen vor:
    ...
 -}
 
 
+
+
+anb1,anb2,anbf1,anbf2 :: Anbieter
+anb1 = [(H1,sort1),
+        (H2,sort2),
+        (H3,sort3),
+        (H4,sort4),
+        (H5,sort5),
+        (H6,sort6),
+        (H7,sort7),
+        (H8,sort8),
+        (H9,sort9),
+        (H10,sort10)
+        ]
+anb2 = [(H1,sort1),
+        (H2,sort2),
+        (H3,sort3)
+        ]
+anbf1 =[(H1,sort1),
+        (H2,sort2),
+        (H3,sort3),
+        (H4,sort4),
+        (H5,sort5),
+        (H6,sort6),
+        (H6,sort6),--duplicate
+        (H7,sort7),
+        (H8,sort8),
+        (H9,sort9),
+        (H10,sort10)
+        ]
+anbf2 =[(H1,sort1),
+        (H2,sort2),
+        (H3,sort3),
+        (H4,sort4),
+        (H5,sort5),
+        (H6,sortf1), -- faulty Sortiment
+        (H7,sort7),
+        (H8,sort8),
+        (H9,sort9),
+        (H10,sort10)]
+sort1, sort2, sort3, sort4, sort5, sort6, sort7, sort8, sort9, sort10, sortf1, sortf2 :: Sortiment
+sort1  = [(M M1,ds2),(M M2,ds2),(M M3,ds2),(M M4,ds2),(M M5,ds2),(T T1,ds2),(T T2,ds2),(T T3,ds2),(T T4,ds2),(S S1,ds2),(S S2,ds2),(S S3,ds2)]
+sort2  = [(M M1,ds2),(M M2,ds2),(M M3,ds2),(M M4,ds2),(M M5,ds2)]
+sort3  = [(T T1,ds2),(T T2,ds2),(T T3,ds2),(T T4,ds2)]
+sort4  = [(S S1,ds2),(S S2,ds2),(S S3,ds2)]
+sort5  = [(M M1,ds2),(T T1,ds2),(S S1,ds2)]
+sort6  = [(M M2,ds2),(T T2,ds2),(S S2,ds2)]
+sort7  = [(M M3,ds2),(T T3,ds2),(S S3,ds2)]
+sort8  = [(M M4,ds2),(T T4,ds2)]
+sort9  = [(M M5,ds2)]
+sort10 = [(M M5,ds2),(M M1,ds2),(M M5,ds2),(M M5,ds2)]
+sortf1 = [(M M5,ds2),(M M1,ds2),(M M5,ds2)] --2* M M5
+sortf2 = [(M M1,ds2),(M M2,ds2),(M M3,ds2),(M M4,ds2),(M M5,ds2),(T T1,ds2),(T T2,ds2),(T T3,ds2),(T T3,ds2),(T T4,ds2),(S S1,ds2),(S S2,ds2),(S S3,ds2)] --2* T t3
+
+ds0, ds1, ds2, ds3, ds4, ds5 :: Datensatz
+
+ds0 = DS 0   0 lab0 Kein_Skonto
+ds1 = DS 201 2 lab1 ZehnProzent
+ds2 = DS 103 3 lab2 FuenfProzent
+ds3 = DS 99  4 lab3 FuenfProzent
+ds4 = DS 102 5 lab4 DreiProzent
+ds5 = DS 99  6 lab5 Kein_Skonto
+
+lab0, lab1, lab2, lab3, lab4, lab5, lab6, labf :: Lieferausblick
+lab0 = []
+lab1 = [(LF Q1 2023, 10),
+        (LF Q2 2023, 11),
+        (LF Q3 2023, 12),
+        (LF Q4 2023, 13),
+        (LF Q4 2023, 13),--duplicate
+        (LF Q1 2024, 14),
+        (LF Q2 2024, 15),
+        (LF Q3 2024, 16),
+        (LF Q4 2024, 17),
+        (LF Q1 2025, 18),
+        (LF Q2 2025, 19),
+        (LF Q3 2025, 20),
+        (LF Q4 2025, 21)
+        ]
+lab2 = [(LF Q1 2023,  1),
+        (LF Q2 2023,  3),
+        (LF Q3 2023,  1),
+        (LF Q4 2023,  3),
+        (LF Q1 2024,  1),
+        (LF Q2 2024,  2),
+        (LF Q3 2024,  1),
+        (LF Q4 2024,  4),
+        (LF Q1 2025,  2),
+        (LF Q1 2025,  2),--duplicate
+        (LF Q2 2025,  1),
+        (LF Q3 2025,  1),
+        (LF Q4 2025,  1)
+        ]
+lab3 = [(LF Q1 2023, 10),
+        (LF Q2 2023,  9),
+        (LF Q3 2023,  8),
+        (LF Q4 2023,  7),
+        (LF Q1 2024,  6),
+        (LF Q2 2024,  5),
+        (LF Q3 2024,  4),
+        (LF Q4 2024,  3),
+        (LF Q1 2025,  2),
+        (LF Q1 2025,  2),--duplicate
+        (LF Q1 2025,  2),--duplicate
+        (LF Q2 2025,  1),
+        (LF Q3 2025,  0),
+        (LF Q4 2025,  0)
+        ]
+lab4 = [(LF Q1 2023,  0),
+        (LF Q2 2023,  0),
+        (LF Q3 2023,  0),
+        (LF Q4 2023,  0),
+        (LF Q1 2024,  0),
+        (LF Q2 2024,  0),
+        (LF Q3 2024,  0),
+        (LF Q4 2024,  0),
+        (LF Q1 2025,  0),
+        (LF Q2 2025,  0),
+        (LF Q3 2025,  0),
+        (LF Q4 2025,  0),
+        (LF Q1 2023,  0),--duplicate
+        (LF Q2 2023,  0),--duplicate
+        (LF Q3 2023,  0),--duplicate
+        (LF Q4 2023,  0),--duplicate
+        (LF Q1 2024,  0),--duplicate
+        (LF Q2 2024,  0),--duplicate
+        (LF Q3 2024,  0),--duplicate
+        (LF Q4 2024,  0),--duplicate
+        (LF Q1 2025,  0),--duplicate
+        (LF Q2 2025,  0),--duplicate
+        (LF Q3 2025,  0),--duplicate
+        (LF Q4 2025,  0)--duplicate
+        ]
+lab5 = [(LF Q3 2024, 0),--shuffled
+        (LF Q3 2025, 0),
+        (LF Q2 2025, 0),
+        (LF Q4 2023, 0),
+        (LF Q1 2023, 0),
+        (LF Q2 2023, 0),
+        (LF Q4 2025, 0),
+        (LF Q1 2025, 0),
+        (LF Q1 2024, 0),
+        (LF Q2 2024, 0),
+        (LF Q3 2023, 0),
+        (LF Q4 2024, 0)
+        ]
+lab6 = [(LF Q3 2024, 0),--shuffled
+        (LF Q3 2025, 0),
+        (LF Q2 2025, 0),
+        (LF Q4 2023, 0),
+        (LF Q1 2023, 0),
+        (LF Q2 2023, 0),
+        (LF Q4 2025, 0),
+        (LF Q1 2025, 0),
+        (LF Q1 2024, 0),
+        (LF Q2 2024, 0),
+        (LF Q3 2023, 0),
+        (LF Q4 2024, 0)
+        ]
+labf = [(LF Q1 2023, 0),
+        (LF Q2 2023, 0),
+        (LF Q3 2023, 0),
+        (LF Q4 2023, 0),
+        (LF Q4 2023, 1),--duplicate, differing
+        (LF Q1 2024, 0),
+        (LF Q2 2024, 0),
+        (LF Q3 2024, 0),
+        (LF Q4 2024, 0),
+        (LF Q1 2025, 0),
+        (LF Q2 2025, 0),
+        (LF Q3 2025, 0),
+        (LF Q4 2025, 0)]
 
