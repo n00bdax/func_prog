dbeca890556893108d9756482c67c35cb296cfe4
n00bdax <n00bdax@hotmail.com>
2022-11-17 13:32:23 +0100

    merge

--- src/Angabe5.hs
+++ src/Angabe5.hs
@@ -3,12 +3,13 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use camelCase" #-}
+{-# HLINT ignore "Use sortOn" #-}
 module Angabe5 where
 
 import Data.Bifunctor
-import Data.List (sort)
+import Data.List
 import Data.Maybe
-import GHC.Stack (HasCallStack)
+import Data.Ord
 
 {- 1. Vervollstaendigen Sie gemaess Angabentext!
    2. Vervollständigen Sie auch die vorgegebenen Kommentaranfänge!
@@ -91,24 +92,28 @@ type Suchanfrage = Typ
 
 ----------------- helper functions ----------------
 
-toData :: Typ -> Anbieter -> [(Haendler, Datensatz)]
-toData typ (A anbieter) =
-  mapMaybe
-    ( \(x, Sort y) ->
-        ( \case
-            (_, Nothing) -> Nothing
-            (v, w) -> Just (v, fromJust w)
-        )
-          ( x,
-            ( \case
-                [] -> Nothing
-                v -> Just (head v)
-            )
-              . map snd
-              $ filter (\g -> fst g == typ) y
-          )
-    )
-    anbieter
+toData :: Typ -> [(Haendler, Sortiment)] -> [(Haendler, Datensatz)]
+toData typ = mapMaybe $ \(x, Sort y) -> f (x, lookup typ y)
+  where
+    f (x, Just y) = Just (x, y)
+    f _ = Nothing
+
+-- toData typ =
+--   mapMaybe $
+--     \(x, Sort y) ->
+--       ( \case
+--           (v, Just w) -> Just (v, w)
+--           (_, Nothing) -> Nothing
+--       )
+--         ( x,
+--           ( \case
+--               v : _ -> Just v
+--               _ -> Nothing
+--           )
+--             . map snd
+--             . filter (\g -> fst g == typ)
+--             $ y
+--         )
 
 gPrice :: Datensatz -> Nat1
 gPrice (DS x _ _ _) = x
@@ -145,7 +150,8 @@ gStockBy (DS _ _ (LA x) _) lff =
       (a : _) -> (snd a)
       _ -> 0
   )
-    $ filter (\a -> fst a == lff) x
+    . filter (\a -> fst a == lff)
+    $ x
 gStockBy _ _ = 0
 
 getSkonto :: Datensatz -> Skonto
@@ -160,10 +166,20 @@ hasDuplicates :: Eq a => [a] -> Bool
 hasDuplicates (x : xs) = elem x xs || hasDuplicates xs
 hasDuplicates _ = False
 
+check1 :: Anbieter -> Anbieter
+check1 a
+  | ist_nwgf a = wgf_fehler a
+  | otherwise = a
+
+check2 :: Anbieter -> Anbieter
+check2 a
+  | ist_nwgf a = error "Anbieterargumentfehler"
+  | otherwise = a
+
 class Wgf a where -- Wgf fuer `wohlgeformt'
   ist_wgf :: a -> Bool -- ist_wgf fuer `ist wohlgeformt'
   ist_nwgf :: a -> Bool -- ist_nwgf fuer `ist nicht wohlgeformt'
-  wgf_fehler :: a -> b
+  wgf_fehler :: a -> a
 
   -- Protoimplementierungen
   ist_wgf = not . ist_nwgf
@@ -183,40 +199,36 @@ instance Wgf Lieferausblick where
       subCheck _ _ = True
   ist_wgf _ = True
 
-  wgf_fehler :: Lieferausblick -> b
+  wgf_fehler :: Lieferausblick -> a
   wgf_fehler = error "Ausblickfehler"
 
 instance Wgf Sortiment where
   ist_nwgf :: Sortiment -> Bool
   ist_nwgf (Sort x) = hasDuplicates (map fst x) || any (ist_nwgf . gLA . snd) x
 
-  wgf_fehler :: Sortiment -> b
+  wgf_fehler :: Sortiment -> a
   wgf_fehler = error "Sortimentfehler"
 
 instance Wgf Anbieter where
   ist_nwgf :: Anbieter -> Bool
   ist_nwgf (A x) = hasDuplicates (map fst x) || any (ist_nwgf . snd) x
 
-  wgf_fehler :: Anbieter -> b
+  wgf_fehler :: Anbieter -> a
   wgf_fehler = error "Anbieterfehler"
-
-instance Wgf Bool where
-  ist_wgf :: Bool -> Bool
-  ist_wgf = const True
-  wgf_fehler :: Bool -> b
-  wgf_fehler = error "Anbieterargumentfehler"
 
 -- Aufgabe A.5
 
 type Haendlerliste = [Haendler]
 
 sofort_lieferfaehig :: Suchanfrage -> Anbieter -> Haendlerliste
-sofort_lieferfaehig typ (A anbieter)
-  | null anbieter = []
-  | ist_nwgf (A anbieter) = wgf_fehler (A anbieter)
-  | otherwise =
-      reverse . sort . map fst . filter (\(_, x) -> gStock x > 0) $
-        toData typ (A anbieter)
+sofort_lieferfaehig typ =
+  sortBy (comparing Down)
+    . map fst
+    . filter (\(_, x) -> gStock x > 0)
+    . toData typ
+    . (\(A x) -> x)
+    . (\x -> if ist_nwgf x then wgf_fehler x else x)
+
 
 -- Aufgabe A.6
 
@@ -227,11 +239,12 @@ type Gesamtpreis = Nat0
 sofort_erhaeltliche_Stueckzahl :: Suchanfrage -> Anbieter -> (Stueckzahl, Gesamtpreis)
 sofort_erhaeltliche_Stueckzahl typ (A anbieter)
   | null anbieter = (0, 0)
-  | ist_nwgf (A anbieter) = wgf_fehler True
+  | ist_nwgf (A anbieter) = error "Anbieterargumentfehler"
   | otherwise =
       foldl (\(a, b) (c, d) -> (a + c, b + d)) (0, 0)
         . map (\(_, x) -> (gStock x, gPrice x * gStock x))
-        $ toData typ (A anbieter)
+        . toData typ
+        $ anbieter
 
 -- Aufgabe A.7
 
@@ -240,16 +253,16 @@ type Preis = EUR
 guenstigste_Lieferanten :: Suchanfrage -> Lieferfenster -> Anbieter -> Maybe Haendlerliste
 guenstigste_Lieferanten typ lff (A anbieter)
   | null anbieter = Nothing
-  | ist_nwgf (A anbieter) = wgf_fehler True
+  | ist_nwgf (A anbieter) = error "Anbieterargumentfehler"
   | otherwise =
       (\x -> if null x then Nothing else Just x)
-        . reverse
-        . sort
+        . sortBy (comparing Down)
         . map fst
         . trim2MinSnd
         . map (second gPrice)
         . filter (\x -> gStockBy (snd x) lff > 0)
-        $ toData typ (A anbieter)
+        . toData typ
+        $ anbieter
 
 -- Aufgabe A.8
 
@@ -258,11 +271,12 @@ type RabattierterPreis = EUR
 guenstigste_Lieferanten_im_Lieferfenster :: Suchanfrage -> Lieferfenster -> Stueckzahl -> Anbieter -> [(Haendler, RabattierterPreis)]
 guenstigste_Lieferanten_im_Lieferfenster typ lff n (A anbieter)
   | null anbieter = []
-  | ist_nwgf (A anbieter) = wgf_fehler True
+  | ist_nwgf (A anbieter) = error "Anbieterargumentfehler"
   | otherwise =
-      reverse
-        . sort
+      sortBy
+        (comparing $ Down . fst)
         . trim2MinSnd
         . map (\(x, y) -> (x, EUR $ (\a -> 10 * ceiling (a / 10)) (gPriceRed y * fromIntegral n)))
-        . filter (\(_, x) -> gStockBy x lff >= n)
-        $ toData typ (A anbieter)
+        . filter (\x -> gStockBy (snd x) lff >= n)
+        . toData typ
+        $ anbieter
