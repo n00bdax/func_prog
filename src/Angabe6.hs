eb60040b5f9d9f02e56890f2eff00e8fc66de462
n00bdax <julian.schwendinger@gmail.com>
2022-11-18 23:49:18 +0100

    A6 fucked around making Typ Bounded

--- src/Angabe6.hs
+++ src/Angabe6.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+{-# LANGUAGE InstanceSigs #-}
 module Angabe6 where
 
 {- 1. Vervollstaendigen Sie gemaess Angabentext!
@@ -11,61 +14,121 @@ type Nat0    = Int     -- Natürliche Zahlen beginnend mit 0
 type Nat1    = Int     -- Natürliche Zahlen beginnend mit 1
 
 newtype EUR  = EUR { euro :: Nat1 }
+   deriving (Eq,Ord,Show)
 
-data Skonto  = Kein_Skonto 
-               | DreiProzent  
-               | FuenfProzent 
+data Skonto  = Kein_Skonto
+               | DreiProzent
+               | FuenfProzent
                | ZehnProzent
+   deriving (Eq,Ord,Show,Enum,Bounded)
 
 data Waschmaschine    = M1 | M2 | M3 | M4 | M5
+   deriving (Eq,Ord,Show,Enum,Bounded)
+
 data Waeschetrockner  = T1 | T2 | T3 | T4
+   deriving (Eq,Ord,Show,Enum,Bounded)
+
 data Waescheschleuder = S1 | S2 | S3
+   deriving (Eq,Ord,Show,Enum,Bounded)
 
 data Typ = M Waschmaschine
            | T Waeschetrockner
            | S Waescheschleuder
+   deriving (Eq, Show, Ord)
 
-data Quartal       = Q1 | Q2 | Q3 | Q4 deriving (Eq,Ord,Show)
+instance Enum Typ where
+  toEnum :: Int -> Typ
+  toEnum x
+      | x <= maxM = M (toEnum x::Waschmaschine)
+      | x <= maxM + maxT +1 = T ((toEnum $ x - maxM-1)::Waeschetrockner)
+      | otherwise = S ((toEnum $ x - maxT - maxM -2)::Waescheschleuder)
+      where
+         maxM = fromEnum (maxBound::Waschmaschine)
+         maxT = fromEnum (maxBound::Waeschetrockner)
+  fromEnum :: Typ -> Int
+  fromEnum (M x) = fromEnum x
+  fromEnum (T x) = fromEnum x + fromEnum (maxBound::Waschmaschine) +1
+  fromEnum (S x) = fromEnum x + fromEnum (maxBound::Waschmaschine) + fromEnum (maxBound::Waeschetrockner) +2
+
+instance Bounded Typ where
+  minBound :: Typ
+  minBound = M minBound
+  maxBound :: Typ
+  maxBound = S maxBound
+
+
+
+typList :: [Typ]
+typList = [minBound..maxBound] ::[Typ]
+
+
+data Quartal       = Q1 | Q2 | Q3 | Q4
+   deriving (Eq,Ord,Show,Enum,Bounded)
+
 data Jahr          = J2023 | J2024 | J2025
+   deriving (Eq,Ord,Show,Enum,Bounded)
+
 data Lieferfenster = LF { quartal :: Quartal,
-                          jahr    :: Jahr 
-                        }
+                          jahr    :: Jahr
+                        }      deriving (Eq,Show,Bounded)
 
-newtype Lieferausblick = LA (Lieferfenster -> Nat0)
+instance Ord Lieferfenster where
+   compare :: Lieferfenster -> Lieferfenster -> Ordering
+   compare (LF a1 a2) (LF b1 b2)
+    | x == EQ = compare a1 b1
+    | otherwise = x
+    where
+      x = compare a2 b2
 
-data Datensatz 
-   = DS { preis_in_euro :: Nat1,
-          sofort_lieferbare_stueckzahl :: Nat0,
+instance Enum Lieferfenster where
+
+  toEnum :: Int -> Lieferfenster
+  toEnum n = LF  (toEnum (mod n 4)) (toEnum (div n 4))
+
+  fromEnum :: Lieferfenster -> Int
+  fromEnum (LF q y)= fromEnum y * 4 + fromEnum q
+
+lieferfensterList :: [Lieferfenster]
+lieferfensterList = [minBound..maxBound]
+
+data Datensatz
+   = DS { preis_in_euro                        :: Nat1,
+          sofort_lieferbare_stueckzahl         :: Nat0,
           lieferbare_stueckzahl_im_Zeitfenster :: Lieferausblick,
-          skonto :: Skonto
+          skonto                               :: Skonto
         }
      | Nicht_im_Sortiment
 
-newtype Sortiment = Sort (Typ -> Datensatz)
 
 data Haendler = H1 | H2 | H3 | H4 | H5 | H6 | H7 | H8 | H9 | H10
+   deriving (Eq,Ord,Show,Enum,Bounded)
 
+haendlerList :: [Haendler]
+haendlerList = [minBound..maxBound]
+
+newtype Sortiment = Sort (Typ -> Datensatz)
 newtype Anbieter = A (Haendler -> Sortiment)
+newtype Lieferausblick = LA (Lieferfenster -> Nat0)
 
-type Suchanfrage = Typ  
+type Suchanfrage = Typ
 
 class Wgf a where                -- Wgf fuer `wohlgeformt'
  ist_wgf    :: a -> Bool         -- ist_wgf fuer `ist wohlgeformt'
  ist_nwgf   :: a -> Bool         -- ist_nwgf fuer `ist nicht wohlgeformt'
  wgf_fehler :: a -> a
  -- Protoimplementierungen
- ist_wgf x  = not (ist_nwgf x)
- ist_nwgf x = not (ist_wgf x)
- wgf_fehler = \x -> error "Argument fehlerhaft"
+ ist_wgf  = not.ist_nwgf
+ ist_nwgf = not.ist_wgf
+ wgf_fehler = error "Argument fehlerhaft"
 
 
 -- Aufgabe A.1
 
-wg_la :: Lieferausblick -> [(Lieferfenster,Nat0)]
+-- wg_la :: Lieferausblick -> [(Lieferfenster,Nat0)]
 
-wg_so :: Sortiment -> [(Typ,Datensatz)]
+-- wg_so :: Sortiment -> [(Typ,Datensatz)]
 
-wg_ab :: Anbieter ->  [(Haendler,Sortiment)
+-- wg_ab :: Anbieter ->  [(Haendler,Sortiment)]
 
 
 {- Knapp, aber gut nachvollziehbar gehen die Implementierungen folgendermassen vor:
@@ -105,7 +168,7 @@ sofort_lieferfaehig _ _ = error "Noch nicht implementiert!"
 
 type Stueckzahl  = Nat0
 type Gesamtpreis = Nat0
- 
+
 sofort_erhaeltliche_Stueckzahl :: Suchanfrage -> Anbieter -> (Stueckzahl,Gesamtpreis)
 sofort_erhaeltliche_Stueckzahl _ _ = error "Noch nicht implementiert!"
 
